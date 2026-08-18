ca8eea0a86e7a93d435748d96708233137e4e57a
Kirubakaran.A <a.karankiruba6607@gmail.com>
2026-08-18 11:43:16 +0530

    A Spring Boot REST API for managing student records with CRUD operations using Java, Spring Boot, Spring Data JPA, Hibernate, and MySQL.

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StudentManagementApp app = new StudentManagementApp();
+        app.start();
+
+    }
+
+}
